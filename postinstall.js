43be88a3acbc07b1775e42d07f792a76a09a4603
André Costa Lima <andreclima.pt@gmail.com>
2020-12-15 20:41:02 +0000

    fix: patch zora automatically in development and release

--- postinstall.js
+++ postinstall.js
@@ -5,4 +5,13 @@
 const path = require('path');
 const execa = require('execa');
 
-execa.sync('patch', ['-p1', '-i', path.join(__dirname, 'patches', 'zora+4.0.1.patch')]);
+const isDependency = path.basename(path.dirname(process.cwd())) === 'node_modules';
+const cwd = isDependency ? path.join(process.cwd(), '..', '..') : process.cwd();
+
+const patchZora = () => {
+    const zoraPatchFilePath = path.join(__dirname, 'patches', 'zora+4.0.1.patch');
+
+    execa.sync('patch', ['-p1', '-i', zoraPatchFilePath], { cwd });
+};
+
+patchZora();
